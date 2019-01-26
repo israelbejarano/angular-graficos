650b1c243cded5e070b6ba6e25630abbc51c9b86
israel <israel.bejarano.a@gmail.com>
2019-01-26 13:03:25 +0100

    Doughnut Chart example

---
+++ src/app/components/donut/donut.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donut',
+  templateUrl: './donut.component.html',
+  styles: []
+})
+export class DonutComponent {
+
+  public doughnutChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: number[] = [350, 450, 100];
+  public doughnutChartType = 'doughnut';
+
+  // events
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
+
+  public randomize() {
+    this.doughnutChartData = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100)
+    ];
+  }
+}
