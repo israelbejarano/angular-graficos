ce4a26614e0e9ad6132e2766a20e7c24c06be155
israel <israel.bejarano.a@gmail.com>
2019-01-26 13:25:25 +0100

    imports for execute example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,23 @@ import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
 import { LineaComponent } from './components/linea/linea.component';
+import { BarraComponent } from './components/barra/barra.component';
+import { DonutComponent } from './components/donut/donut.component';
+import { RadarComponent } from './components/radar/radar.component';
+import { PieComponent } from './components/pie/pie.component';
+import { PolarAreaComponent } from './components/polar-area/polar-area.component';
+import { DynamicComponent } from './components/dynamic/dynamic.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LineaComponent
+    LineaComponent,
+    BarraComponent,
+    DonutComponent,
+    RadarComponent,
+    PieComponent,
+    PolarAreaComponent,
+    DynamicComponent
   ],
   imports: [
     BrowserModule,
